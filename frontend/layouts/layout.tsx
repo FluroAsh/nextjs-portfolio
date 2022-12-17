4831272631ba79785254c813f967e282c6a4a6aa
Ashley Thompson <workashleythompson@gmail.com>
2022-12-17 17:27:15 +1100

    update import for navbar

--- frontend/layouts/layout.tsx
+++ frontend/layouts/layout.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from 'react'
-import Navbar from '../components/Navbar'
+import Navbar from 'components/Navbar'
 
 export function Layout({ children }: { children: ReactNode }) {
   return (
