7c5c910de13155e5d82983684a1249bee4e05b30
Ashley Thompson <workashleythompson@gmail.com>
2022-12-17 17:19:04 +1100

    fixes head -- refactoring title later

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -4,10 +4,8 @@ export default function Document() {
   return (
     <Html lang="en">
       {/* TODO: Add color below as a custom color to TW CSS config */}
-      <body className="dark:bg-[#2d373c] min-h-screen">
-        <Head>
-          <title>Ashley Thompson</title>
-        </Head>
+      <Head></Head>
+      <body className="dark:bg-[#2d373c] min-h-screen dark:text-white">
         <Main />
         <NextScript />
       </body>
