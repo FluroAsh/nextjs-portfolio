e174d1cf559cef7eeb80ee0aac652023373c360a
Ashley Thompson <workashleythompson@gmail.com>
2022-12-19 00:39:18 +1100

    tweaks styles to be passed as a styles object

--- frontend/components/Navbar/component.tsx
+++ frontend/components/Navbar/component.tsx
@@ -6,14 +6,16 @@ export const Navbar = () => {
   const [isScrolling] = useScrolling()
 
   const styleScrolling = isScrolling
-    ? 'dark:bg-zinc-900/50'
-    : 'bg:transparent border-transparent'
+
+  const styles = {
+    container: isScrolling
+      ? 'dark:bg-zinc-900/50'
+      : 'bg:transparent border-transparent'
+  }
 
   return (
-    // WHEN position is not at the top of the document
-    // change opacity to 80% and include backdrop filter effects for blur
     <div
-      className={`sticky top-0 z-99 w-full transition-all backdrop-blur border-b border-b-slate-500/50 ${styleScrolling} drop-shadow-md`}
+      className={`sticky top-0 z-99 w-full transition-all backdrop-blur border-b border-b-slate-500/50 ${styles.container} drop-shadow-md`}
     >
       <nav className="flex max-w-screen-lg p-3 mx-auto" data-testid="navbar">
         <span
@@ -27,6 +29,8 @@ export const Navbar = () => {
           id="navbar-links"
           className="flex items-center justify-center w-1/3 gap-5"
         >
+          {/* TODO: Map over items to render these links */}
+          {/* Title, to, target (eg _blank) */}
           <Link title="Go to Homepage" href="/" className="text-sm uppercase">
             Home
           </Link>
@@ -46,7 +50,7 @@ export const Navbar = () => {
           <input
             type="text"
             placeholder="Search"
-            className="max-w-[150px] px-3 py-1 ml-auto rounded outline-none focus:outline-sky-500/80 bg:transparent"
+            className="max-w-[150px] px-3 py-1 ml-auto rounded outline-none focus:outline-sky-500/80 outline-offset-[-1px] bg:transparent"
           ></input>
         </div>
       </nav>
