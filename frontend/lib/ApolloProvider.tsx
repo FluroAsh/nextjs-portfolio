1c7686e6675fb2bd0fdc8b4aca9afcce8ffd8a34
Ashley Thompson <workashleythompson@gmail.com>
2022-12-18 23:44:45 +1100

    adds ApolloProvider HOC

---
+++ frontend/lib/ApolloProvider.tsx
@@ -0,0 +1,18 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider as Apollo
+} from '@apollo/client'
+
+import React, { ReactNode } from 'react'
+
+function ApolloProvider({ children }: { children: ReactNode }) {
+  const client = new ApolloClient({
+    uri: `${process.env.NEXT_PUBLIC_STRAPI_API_URL}/graphql`,
+    cache: new InMemoryCache()
+  })
+
+  return <Apollo client={client}>{children}</Apollo>
+}
+
+export default ApolloProvider
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,17 +1,12 @@
 import '../main.css'
 
+import ApolloProvider from 'lib/ApolloProvider'
 import type { AppProps } from 'next/app'
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
-
-const client = new ApolloClient({
-  uri: `${process.env.NEXT_PUBLIC_STRAPI_API_URL}/graphql`,
-  cache: new InMemoryCache()
-})
 
 function BlogApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <ApolloProvider client={client}>
+      <ApolloProvider>
         <Component {...pageProps} />
       </ApolloProvider>
     </>
