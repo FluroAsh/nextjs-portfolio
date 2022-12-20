58588b74b1c80afef2e5ec4396a0976f76da36c3
Ashley Thompson <workashleythompson@gmail.com>
2022-12-20 18:49:53 +1100

    updates Layout component & adds interface for ctx slug

--- frontend/pages/blog/[slug].tsx
+++ frontend/pages/blog/[slug].tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { useRouter } from 'next/router'
 import { GetStaticProps, GetStaticPaths } from 'next'
-
+import { ParsedUrlQuery } from 'querystring'
 import Error from 'pages/_error'
 import { GET_POST_SLUGS, GET_POST } from 'pages/api'
 import { IPostData } from 'interfaces'
+
 import { markdownToHtml } from 'lib/markdownToHtml'
+import { Layout } from 'components/layout'
 import Head from 'next/head'
 
 export default function Post({ post }: IPostData) {
@@ -15,10 +17,10 @@ export default function Post({ post }: IPostData) {
   }
 
   return (
-    <>
-      {/* <Head>
-        <title>{`AT | ${post.title}`}</title>
-      </Head> */}
+    <Layout pageType="blog">
+      <Head>
+        <title>{post?.title}</title>
+      </Head>
       {router.isFallback ? (
         <p>Loading...</p>
       ) : (
@@ -31,15 +33,17 @@ export default function Post({ post }: IPostData) {
           <article dangerouslySetInnerHTML={{ __html: post.content }}></article>
         </div>
       )}
-    </>
+    </Layout>
   )
 }
 
-export const getStaticProps: GetStaticProps = async ({
-  params,
-  preview = null
-}: any) => {
-  const { posts } = await GET_POST(params.slug)
+interface IParams extends ParsedUrlQuery {
+  slug: string
+}
+
+export const getStaticProps: GetStaticProps = async (ctx, preview = false) => {
+  const { slug } = ctx.params as IParams
+  const { posts } = await GET_POST(slug, preview)
   const content =
     (await markdownToHtml(posts.data[0]?.attributes?.content)) || ''
 
