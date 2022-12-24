b4887cf29026130a613a889f5f28f5aa207c2103
Ashley Thompson <workashleythompson@gmail.com>
2022-12-24 21:25:45 +1100

    adds HeaderLogo component

---
+++ frontend/src/components/HeaderLogo.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import React from 'react'
+
+export const HeaderLogo = () => {
+  return (
+    <Link
+      href="/"
+      className="flex items-center mr-2 text-2xl font-thin text-bold"
+    >
+      <div className="text-orange-500 dark:text-sky-500">
+        <span className="text-black dark:text-white">a</span>
+        |thompson
+      </div>
+    </Link>
+  )
+}
+
+export default HeaderLogo
