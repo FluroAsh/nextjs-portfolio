bab7ecbf7e995b01bd2ff700cbfcce289b6d60bc
Ashley Thompson <workashleythompson@gmail.com>
2022-12-24 21:40:12 +1100

    adds the custom color to dark bg

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -2,10 +2,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html className="dark" lang="en">
       {/* TODO: Add color below as a custom color to TW CSS config */}
       <Head></Head>
-      <body className="bg-orange-50 dark:bg-[#2d373c] min-h-screen dark:text-white">
+      <body className="min-h-screen bg-orange-50 dark:bg-dark-background-primary dark:text-white">
         <Main />
         <NextScript />
       </body>
