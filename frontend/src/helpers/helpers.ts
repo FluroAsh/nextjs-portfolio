8ea035bc47d3eb876c89137f517b0b5970954ef3
Ashley Thompson <workashleythompson@gmail.com>
2022-12-24 23:05:40 +1100

    removes old theme helper

--- frontend/src/helpers/helpers.ts
+++ frontend/src/helpers/helpers.ts
@@ -1,11 +1,3 @@
-export const handleTheme = (theme: string) => {
-  if (!theme) {
-    return 'dark'
-  }
-
-  return theme === 'dark' ? 'light' : 'dark'
-}
-
 export function truncateTitle(title: string) {
   return title.length >= 16 ? `${title.substring(0, 13)}...` : title
 }
