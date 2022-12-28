e77a921b0a027d37c04d9efe6cc10f667cb71e6a
Ashley Thompson <workashleythompson@gmail.com>
2022-12-28 18:34:39 +1100

    updates to include layout for errors

--- frontend/src/pages/_error.tsx
+++ frontend/src/pages/_error.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
+import { Layout } from 'components/layout'
 
 export interface IErrorMap {
   [code: string]: string
@@ -12,23 +13,25 @@ export const ERROR_MAP: IErrorMap = {
 
 export function Error({ statusCode }: { statusCode: number }) {
   return (
-    <div className="flex items-center justify-center dark:text-white h-[calc(100vh-57px)] ">
-      <div className="flex flex-col items-center justify-center p-6 border rounded-lg w-100 dark:bg-slate-500/50 backdrop-blur border-slate-500/50">
-        {/* TODO: Replace with an SVG/Img */}
-        <span className="mb-4 text-5xl">⚠️</span>
-        <div className="flex items-center content-center text-lg">
-          <span className="flex items-center mr-2 after:border-r-2 after:ml-2 after:border-slate-400 after:h-8 after:inline-block">
-            Error {statusCode} &mdash; {ERROR_MAP[statusCode]}
-          </span>
-          <Link
-            className="transition-colors duration-300 dark:text-slate-300 hover:text-sky-500"
-            href="/"
-          >
-            Go Home
-          </Link>
+    <Layout pageType="simple">
+      <div className="flex items-center justify-center dark:text-white h-[calc(100vh-57px)] ">
+        <div className="flex flex-col items-center justify-center p-6 border rounded-lg w-100 dark:bg-slate-500/50 backdrop-blur border-slate-500/50">
+          {/* TODO: Replace with an SVG/Img */}
+          <span className="mb-4 text-5xl">⚠️</span>
+          <div className="flex items-center content-center text-lg">
+            <span className="flex items-center mr-2 after:border-r-2 after:ml-2 after:border-slate-400 after:h-8 after:inline-block">
+              Error {statusCode} &mdash; {ERROR_MAP[statusCode]}
+            </span>
+            <Link
+              className="transition-colors duration-300 dark:text-slate-300 hover:text-sky-500"
+              href="/"
+            >
+              Go Home
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </Layout>
   )
 }
 
