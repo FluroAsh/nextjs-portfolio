055136a01c72e3701047b56c193632bd07431f3c
Ashley Thompson <workashleythompson@gmail.com>
2022-12-28 19:10:40 +1100

    changes container bg color

--- frontend/src/components/ThemeToggle/ThemeToggle.tsx
+++ frontend/src/components/ThemeToggle/ThemeToggle.tsx
@@ -6,13 +6,19 @@ import { THEME_DARK, THEME_LIGHT } from 'constants/theme'
 
 const ThemeToggle = () => {
   const { theme, setTheme } = useTheme()
+  const isDark = theme === 'dark'
 
   const handleTheme = () =>
     setTheme(theme === THEME_DARK ? THEME_LIGHT : THEME_DARK)
 
   return (
-    <div className="flex justify-center w-[25px] h-[25px]">
-      <button onClick={handleTheme}>☀️</button>
+    <div
+      className={`transition-colors duration-300 flex align-center justify-center w-[25px] h-[25px] bg-slate-500 ${
+        isDark ? 'bg-orange-200' : 'bg-dark-background-primary'
+      } rounded drop-shadow-sm`}
+    >
+      {/* TODO: Add icons instead of emojis */}
+      <button onClick={handleTheme}>{isDark ? '🌞' : '🌙'}</button>
     </div>
   )
 }
