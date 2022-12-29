80969f7601aa1887e7d16c26df408c161a9db8c6
Ashley Thompson <workashleythompson@gmail.com>
2022-12-29 13:33:50 +1100

    changes 400 error message

--- frontend/src/constants/errors.ts
+++ frontend/src/constants/errors.ts
@@ -1,6 +1,6 @@
 type ERROR_MAP = { [key: string]: string }
 
 export const ERROR_MAP: ERROR_MAP = {
-  400: 'Unexpected error, please try again',
+  400: 'Unexpected error',
   500: 'Please try again later'
 } as const
