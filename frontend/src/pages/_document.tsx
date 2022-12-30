a1a1b6b201d056a5a375f2f146e84fce0ce7c95b
Ashley Thompson <workashleythompson@gmail.com>
2022-12-30 22:09:45 +1100

    cleaning up

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -3,7 +3,6 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      {/* TODO: Add color below as a custom color to TW CSS config */}
       <Head></Head>
       <body className="min-h-screen text-black transition-all duration-300 bg-orange-50 dark:bg-dark-background-primary dark:text-white">
         <Main />
