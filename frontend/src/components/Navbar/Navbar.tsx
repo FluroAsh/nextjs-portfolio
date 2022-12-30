5012640adba521154e5cb99eb7317a02239f84af
Ashley Thompson <workashleythompson@gmail.com>
2022-12-30 17:57:43 +1100

    tweaks styles & adds placeholder hamburger div

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -15,26 +15,29 @@ const Navbar = () => {
   }
 
   return (
+    // Drop Shadows not rendering correctly when not scrolling for some reason...
     <div
-      className={`sticky z-50 top-0 w-full transition-all border-b dark:border-b-slate-500/50 border-b-orange-300/50 ${styles.container} drop-shadow-sm`}
+      className={`sticky z-50 top-0 w-full transition-all border-b dark:border-b-slate-500/50 border-b-orange-300/50 ${styles.container}`}
     >
       <nav className="flex justify-between max-w-screen-xl p-3 mx-auto">
         <HeaderLogo />
 
-        <div className="flex items-center justify-center gap-5">
-          {/* TODO: Map over items to render these links */}
-          {/* Title, to, target (eg _blank) */}
-          <Link title="Go to Homepage" href="/" className="text-sm">
-            home
-          </Link>
-          <Link title="Go to Ash's Blog" href="/blog" className="text-sm">
-            blog
-          </Link>
-          <Link title="Contact Ash" href="/" className="text-sm">
-            contact
-          </Link>
-          {/* TODO: Create a dark/light mode toggle */}
+        <div className="flex items-center justify-center gap-3">
+          <div className="hidden gap-3 sm:flex">
+            <Link title="Go to Homepage" href="/" className="text-sm">
+              home
+            </Link>
+            <Link title="Go to Ash's Blog" href="/blog" className="text-sm">
+              blog
+            </Link>
+            <Link title="Contact Ash" href="/contact" className="text-sm">
+              contact
+            </Link>
+          </div>
+
           <ThemeToggle />
+          {/* TODO: Hamburger Menu Light: Black, Dark: white/slate-500 */}
+          <div className="w-6 h-6 bg-orange-500 sm:hidden"></div>
         </div>
       </nav>
     </div>
