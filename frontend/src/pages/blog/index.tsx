177eaf4d7f2353a799c53bb2197730b626429b42
Ashley Thompson <workashleythompson@gmail.com>
2022-12-30 19:47:25 +1100

    modify styling for index page

--- frontend/src/pages/blog/index.tsx
+++ frontend/src/pages/blog/index.tsx
@@ -6,8 +6,25 @@ import { Layout } from 'components/layout'
 const Blog = () => {
   return (
     <Layout pageType="basic">
-      <div>Blog</div>
-      <Link href="/blog/placeholder-post">Placeholder Post</Link>
+      <div>
+        <header className="pb-4 border-b dark:border-slate-500 border-orange-300/50">
+          <div className="text-4xl">Latest</div>
+          <span className="dark:text-slate-300 text-neutral-600">
+            The latest collection of my little musings & articles to help you
+            become a better developer.
+          </span>
+        </header>
+
+        {/* Featured Post */}
+        <h2 className="my-2 text-3xl">Featured Post</h2>
+
+        {/* Remaining Posts */}
+        <Link href="/blog/placeholder-post">
+          <h2 className="text-3xl">Placeholder Post</h2>
+        </Link>
+        {/* Published 'x days/years/months ago */}
+        {/* Description */}
+      </div>
     </Layout>
   )
 }
