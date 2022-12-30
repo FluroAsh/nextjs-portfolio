7a3fa1b3e50f9eec2a7082f1f1290f50338a77c2
Ashley Thompson <workashleythompson@gmail.com>
2022-12-30 20:04:25 +1100

    tweaks types

--- frontend/src/lib/types.ts
+++ frontend/src/lib/types.ts
@@ -1,11 +1,12 @@
 export interface IPost extends IPostData {
   className: string | undefined
 }
+
 export interface IPostData {
   title: string
   slug: string
   description: string
-  content?: string
+  content: string
   imageUrl: string
   published: string
   altText: string
