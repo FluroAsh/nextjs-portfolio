7a2228015d1f5125bce588d0a8c7830803112f83
Ashley Thompson <workashleythompson@gmail.com>
2022-12-31 17:03:16 +1100

    adds reminder to include additional meta tags

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head></Head>
+      <Head>
+        {/* TODO: add additional meta tags */}
+      </Head>
       <body className="min-h-screen text-black transition-all duration-300 bg-orange-50 dark:bg-dark-background-primary dark:text-white">
         <Main />
         <NextScript />
