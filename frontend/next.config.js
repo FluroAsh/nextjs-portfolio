d4affbbefe0fb127aed73c4d5cb8f17ccdbcc3ba
Ashley Thompson <workashleythompson@gmail.com>
2022-12-31 19:05:45 +1100

    update next config to include unsplash and localhost "remotePatterns" for images & defines deviceSizes (WIP)

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  reactStrictMode: true
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.unsplash.com'
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost'
+      }
+    ],
+    deviceSizes: [640, 768, 1024, 1280, 1536]
+  }
 }
 
 module.exports = nextConfig
