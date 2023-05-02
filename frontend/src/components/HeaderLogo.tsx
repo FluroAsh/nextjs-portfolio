1cd6810d2cb558e7b14de751be851556c038f394
Ashley Thompson <workashleythompson@gmail.com>
2023-05-03 01:22:23 +1000

    chore: add some drop-shadow to header logo text 🤙

--- frontend/src/components/HeaderLogo.tsx
+++ frontend/src/components/HeaderLogo.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const HeaderLogo = () => {
   return (
     <Link href="/" className="flex items-center mr-2 text-2xl font-thin">
-      <div className="text-orange-500 dark:text-sky-500">
+      <div className="text-orange-500 drop-shadow-lg dark:text-sky-500">
         <span className="text-black dark:text-white">a</span>
         |thompson
       </div>
