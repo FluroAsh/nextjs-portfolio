bd3d7d32a0152203a0cb93e8aea980ce8c4949e4
Ashley Thompson <workashleythompson@gmail.com>
2023-05-21 21:26:16 +1000

    refactor: changes eror components to uses an errorMessage variable

--- frontend/src/pages/404.tsx
+++ frontend/src/pages/404.tsx
@@ -4,16 +4,18 @@ import Link from 'next/link'
 import Layout from 'components/layout'
 import { ERROR_MAP } from 'constants/errors'
 
+const errorMessage = ERROR_MAP[400]
+
 function FourZeroFour() {
   return (
     <Layout pageType="basic">
-      <div className="flex items-center justify-center dark:text-white h-100vh-minus-navbar">
+      <div className="flex items-center justify-center dark:text-white -h-navbar">
         <div className="flex flex-col items-center justify-center p-6 border rounded-lg w-100 dark:bg-slate-500/50 backdrop-blur border-slate-500/50">
           {/* TODO: Replace with an SVG/Img */}
           <span className="mb-4 text-5xl">⚠️</span>
           <div className="flex items-center content-center text-lg">
             <span className="flex items-center mr-2 after:border-r-2 after:ml-2 after:border-slate-400 after:h-8 after:inline-block">
-              Error 404 &mdash; {ERROR_MAP[400]}
+              Error 404 &mdash; {errorMessage}
             </span>
             <Link
               className="transition-colors duration-300 dark:text-slate-300 hover:text-sky-500"
--- frontend/src/pages/_error.tsx
+++ frontend/src/pages/_error.tsx
@@ -7,15 +7,17 @@ import Layout from 'components/layout'
 import { NextApiResponse } from 'next'
 
 export function Error({ statusCode }: { statusCode: number }) {
+  const errorMessage = ERROR_MAP[statusCode]
+
   return (
     <Layout pageType="basic">
-      <div className="flex items-center justify-center dark:text-white h-100vh-minus-navbar">
+      <div className="flex items-center justify-center dark:text-white -h-navbar">
         <div className="flex flex-col items-center justify-center p-6 border rounded-lg w-100 dark:bg-slate-500/50 backdrop-blur border-slate-500/50">
           {/* TODO: Replace with an SVG/Img */}
           <span className="mb-4 text-5xl">⚠️</span>
           <div className="flex items-center content-center text-lg">
             <span className="flex items-center mr-2 after:border-r-2 after:ml-2 after:border-slate-400 after:h-8 after:inline-block">
-              Error {statusCode} &mdash; {ERROR_MAP[statusCode]}
+              Error {statusCode} &mdash; {errorMessage}
             </span>
             <Link
               className="transition-colors duration-300 dark:text-slate-300 hover:text-sky-500"
