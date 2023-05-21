ee389b5bec66d497378bdc4f6187a285ecb130ff
Ashley Thompson <workashleythompson@gmail.com>
2023-05-21 15:45:06 +1000

    feature: adds WIP error boundary for client errors

---
+++ frontend/src/components/ErrorBoundary.tsx
@@ -0,0 +1,46 @@
+import { Component, type ReactNode, type ErrorInfo } from 'react'
+
+interface ErrorBoundaryProps {
+  fallback?: ReactNode
+  children: ReactNode // remove the Readonly type
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean
+}
+
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props)
+    this.state = { hasError: false }
+  }
+
+  static getDervedStateFromError(error: Error): ErrorBoundaryState {
+    return { hasError: true }
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+    console.log('error caught by error boundary:', error, errorInfo)
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // return this.props.fallback
+      return (
+        <div>
+          <h2>Oops, there is an error!</h2>
+          <button
+            type="button"
+            onClick={() => this.setState({ hasError: false })}
+          >
+            Try again?
+          </button>
+        </div>
+      )
+    }
+
+    return this.props.children
+  }
+}
+
+export default ErrorBoundary
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -4,27 +4,22 @@ import Head from 'next/head'
 import ApolloProvider from 'lib/ApolloProvider'
 import { ThemeProvider } from 'next-themes'
 
-import '../../globals.css'
 import ErrorBoundary from 'components/ErrorBoundary'
 import FourZeroFour from './404'
+import '../../globals.css'
 
 function BlogApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <ErrorBoundary fallback={<FourZeroFour />}>
-        <ThemeProvider attribute="class" defaultTheme="dark">
-          <Head>
-            <meta
-              content="width=device-width, initial-scale=1"
-              name="viewport"
-            />
-          </Head>
-          <ApolloProvider>
-            <Component {...pageProps} />
-          </ApolloProvider>
-        </ThemeProvider>
-      </ErrorBoundary>
-    </>
+    <ErrorBoundary fallback={<FourZeroFour />}>
+      <ThemeProvider attribute="class" defaultTheme="dark">
+        <Head>
+          <meta content="width=device-width, initial-scale=1" name="viewport" />
+        </Head>
+        <ApolloProvider>
+          <Component {...pageProps} />
+        </ApolloProvider>
+      </ThemeProvider>
+    </ErrorBoundary>
   )
 }
 
