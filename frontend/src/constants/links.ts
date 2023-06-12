ca35db6d0b549f12da4e8b5d936fedaddfef35e3
Ashley Thompson <workashleythompson@gmail.com>
2023-06-12 23:15:50 +1000

    chore: updates links for footer icons

--- frontend/src/constants/links.ts
+++ frontend/src/constants/links.ts
@@ -1,3 +1,3 @@
 export const TWITTER_URL = "https://twitter.com/ashleygthompson"
-export const LINKEDIN_URL = "https://www.linkedin.com/in/"
-export const GITHUB_URL = "https://github.com/"
+export const LINKEDIN_URL = "https://www.linkedin.com/in/ashley-thompson-dev/"
+export const GITHUB_URL = "https://github.com/FluroAsh"
