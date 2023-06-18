fb7526f769dd8a3b0288ebf7b77cc8654b5c5316
Ashley Thompson <workashleythompson@gmail.com>
2023-06-18 23:31:43 +1000

    feature: adds onhover opacity to Category links

--- frontend/src/components/Category/Category.tsx
+++ frontend/src/components/Category/Category.tsx
@@ -7,7 +7,9 @@ type CategoryProps = Pick<CategoryAttributes["attributes"], "name" | "slug">
 export const Category: React.FC<CategoryProps> = ({ name, slug }) => {
   return (
     <Link href={`/category/${slug}`}>
-      <div className="font-semibold uppercase">{name}</div>
+      <div className="font-semibold uppercase transition-opacity hover:opacity-80">
+        {name}
+      </div>
     </Link>
   )
 }
