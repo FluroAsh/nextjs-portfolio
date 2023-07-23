8f444f53cea38bb5776e073de8f364d3eaf6fb40
Ashley Thompson <workashleythompson@gmail.com>
2023-07-23 23:46:37 +1000

    refactor: updates Category tag to have a hash before the Tag, not the group of Tags

--- frontend/src/components/Category/Categories.tsx
+++ frontend/src/components/Category/Categories.tsx
@@ -8,7 +8,7 @@ export const Categories: React.FC<{
 }> = ({ categoryData }) => {
   return (
     <div className="flex gap-2 my-2 text-orange-600 sm:my-0 sm:items-center dark:text-sky-400 md:text-lg">
-      {/* TODO: Replace with FA Icon/SVG */}#
+      {/* TODO: Replace with FA Icon/SVG */}
       {categoryData.slice(0, 2).map(({ attributes }) => (
         <CategoryTag
           key={attributes.slug}
--- frontend/src/components/Category/CategoryTag.tsx
+++ frontend/src/components/Category/CategoryTag.tsx
@@ -7,6 +7,7 @@ export const CategoryTag: React.FC<{ name: string; slug: string }> = ({
 }) => (
   <Link href={getSlugPath("category", slug)}>
     <div className="font-semibold tracking-wide uppercase transition-opacity hover:opacity-80">
+      <span className="mr-[3px]">#</span>
       {name}
     </div>
   </Link>
