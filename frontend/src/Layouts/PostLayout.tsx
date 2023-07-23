634a5a230d1924ba9b1cfa7dff83ce8dd96b30ea
Ashley Thompson <workashleythompson@gmail.com>
2023-07-24 01:24:37 +1000

    fix: corrects heroTitle on PostLayout to be 3xl

--- frontend/src/Layouts/PostLayout.tsx
+++ frontend/src/Layouts/PostLayout.tsx
@@ -14,7 +14,7 @@ export const PostLayout: React.FC<{
     <Layout type="blog" title={title}>
       <div className="max-w-screen-lg px-5 mx-auto">
         <header className="py-4 border-b dark:border-slate-500 border-orange-300/50">
-          <div className="text-4xl font-bold text-neutral-800 dark:text-white">
+          <div className="text-3xl font-bold text-neutral-800 dark:text-white">
             {heroTitle ?? defaultHeroTitle}
           </div>
           <span className=" dark:text-slate-300 text-neutral-600">
