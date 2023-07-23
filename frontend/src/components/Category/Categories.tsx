38488d3138ff81c7721a21b9fa1c9b5b83db3757
Ashley Thompson <workashleythompson@gmail.com>
2023-07-23 23:33:10 +1000

    refactor: adds hashtag prefix back to categories

--- frontend/src/components/Category/Categories.tsx
+++ frontend/src/components/Category/Categories.tsx
@@ -2,12 +2,13 @@ import { APICategory } from "types/api-types"
 
 import { CategoryTag } from "./CategoryTag"
 
+/** Returns the first 2 categories from a valid collection of categories. */
 export const Categories: React.FC<{
   categoryData: APICategory[]
 }> = ({ categoryData }) => {
   return (
     <div className="flex gap-2 my-2 text-orange-600 sm:my-0 sm:items-center dark:text-sky-400 md:text-lg">
-      {/* TODO: Replace with FA Icon/SVG */}
+      {/* TODO: Replace with FA Icon/SVG */}#
       {categoryData.slice(0, 2).map(({ attributes }) => (
         <CategoryTag
           key={attributes.slug}
