e475b75738cc0a8184171482e9b0d6f5d0329730
Ashley Thompson <workashleythompson@gmail.com>
2023-07-24 01:18:02 +1000

    feature: adds initial About Me section content

---
+++ frontend/src/components/InfoCards.tsx
@@ -0,0 +1,34 @@
+import * as icon from "../static/icons"
+
+const cardStyles = "absolute top-5 left-5 fill-slate-300/25 w-[75px] h-[75px]"
+const INFO_CARDS = [
+  {
+    heading: "Technology",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    icon: <icon.Twitter className={cardStyles} />,
+  },
+  {
+    heading: "Test 2",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    icon: <icon.Twitter className={cardStyles} />,
+  },
+  {
+    heading: "Test 3",
+    text: "Lorem ipsum dolor sit amet,  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    icon: <icon.Twitter className={cardStyles} />,
+  },
+]
+
+export const InfoCards = () => {
+  return (
+    <div className="grid gap-2 md:grid-rows-1 md:grid-cols-3 sm:cols-1 sm:auto-rows-auto">
+      {INFO_CARDS.map(({ heading, text, icon }) => (
+        <div className="relative p-5 rounded-md shadow-md bg-slate-500">
+          <h1 className="mb-2 text-xl tracking-wide text-center">{heading}</h1>
+          <div className="text-sm ">{text}</div>
+          {icon}
+        </div>
+      ))}
+    </div>
+  )
+}
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head"
 import Layout from "Layouts/layout"
 
 import { HeroBanner } from "components/HeroBanner"
+import { InfoCards } from "components/InfoCards"
 
 const Home: React.FC = () => {
   return (
@@ -12,53 +13,30 @@ const Home: React.FC = () => {
       <Layout type="basic">
         <HeroBanner />
 
-        <div className="px-5">
-          <section id="about-info" className="max-w-screen-xl mx-auto">
-            <h1 className="text-2xl ">Placeholder</h1>
+        <section id="about-me" className="max-w-screen-xl px-5 mx-auto mt-5">
+          <div className="mb-2">
+            <h1 className="text-2xl text-center ">About Me</h1>
+            <h3 className="text-center text-neutral-300">
+              And why I&apos;m the greatest ever
+            </h3>
+          </div>
+          <InfoCards />
+        </section>
 
-            <div className="mt-3">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Pulvinar mattis nunc sed blandit libero. Penatibus et magnis dis
-                parturient montes nascetur ridiculus. Lorem donec massa sapien
-                faucibus et molestie. Odio eu feugiat pretium nibh ipsum
-                consequat nisl vel pretium. Vitae sapien pellentesque habitant
-                morbi tristique senectus. Lorem ipsum dolor sit amet
-                consectetur, adipisicing elit.
-              </p>
-              <p className="mt-3">
-                Possimus, nemo sint sunt, odio optio placeat praesentium
-                molestiae tempore eligendi laudantium et veniam at est ullam
-                porro nisi tempora nam rerum. Consequuntur molestiae voluptates
-                debitis, dolorem expedita repudiandae nemo hic mollitia
-                molestias. Aliquid, dolores? Inventore deleniti doloremque
-                dolorem dolores dignissimos eius eum totam ab debitis a, officia
-                qui nobis quae natus? Id quos architecto asperiores impedit fuga
-                odio esse quae quibusdam magni ipsum non, aliquid perferendis
-                sapiente, quod a explicabo doloremque quidem consequuntur
-                similique magnam incidunt dolorum voluptatibus minima. Cum, nam!
-              </p>
-            </div>
-          </section>
+        {/* TODO: Skils scrolling section (extract to a component) */}
+        <section id="skills" className="max-w-screen-xl mx-auto mt-5">
+          <h1 className="px-5 text-2xl">Skills</h1>
+          <div className="w-100 bg-slate-400 h-80"></div>
+        </section>
 
-          <section id="about-me" className="max-w-screen-xl mx-auto mt-5">
-            <h1 className="text-2xl ">About Me</h1>
-          </section>
+        <section id="projects" className="max-w-screen-xl px-5 mx-auto mt-5">
+          <h1 className="text-2xl ">Projects</h1>
+        </section>
 
-          <section id="skills" className="max-w-screen-xl mx-auto mt-5">
-            <h1 className="text-2xl ">Skills</h1>
-            {/* Skils scrolling section */}
-          </section>
-
-          <section id="projects" className="max-w-screen-xl mx-auto mt-5">
-            <h1 className="text-2xl ">Projects</h1>
-          </section>
-
-          <section id="blog-posts" className="max-w-screen-xl mx-auto mt-5">
-            <h1 className="text-2xl ">Blog Posts</h1>
-            {/* TODO: List thumbnails/Basic info of Blog Posts here */}
-            {/* <ul>
+        <section id="blog-posts" className="max-w-screen-xl px-5 mx-auto mt-5">
+          <h1 className="text-2xl ">Blog Posts</h1>
+          {/* TODO: List thumbnails/Basic info of Blog Posts here */}
+          {/* <ul>
             {posts.map(({ slug, title }) => {
               return (
                 <li>
@@ -67,8 +45,7 @@ const Home: React.FC = () => {
                 )
               })}
             </ul> */}
-          </section>
-        </div>
+        </section>
       </Layout>
     </div>
   )
