7cd3c783a76ff75d8108317726bdeab5efbd8a1c
Ashley Thompson <workashleythompson@gmail.com>
2023-08-26 18:11:19 +1000

    refactor: updates UI/UX for Pagination navigation

--- frontend/src/components/Pagination.tsx
+++ frontend/src/components/Pagination.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react"
 import Link from "next/link"
 import { ROUTE_URL } from "constants/paths"
 
@@ -8,59 +9,12 @@ type PaginationProps = {
   type: PageType
 }
 
-export const Pagination: React.FC<PaginationProps> = ({
-  currentPage,
-  totalPages,
-  type,
-}) => {
-  const PAGE_PATH = {
-    blog: `${ROUTE_URL.BLOG}${ROUTE_URL.PAGE}`,
-    category: `${ROUTE_URL.CATEGORY}${ROUTE_URL.PAGE}`,
-  } as const
+const PAGE_PATH = {
+  blog: `${ROUTE_URL.BLOG}${ROUTE_URL.PAGE}`,
+  category: `${ROUTE_URL.CATEGORY}${ROUTE_URL.PAGE}`,
+} as const
 
-  const pages = Array(totalPages).fill(0)
-
-  return (
-    <div className="flex justify-center w-full mt-4">
-      <div className="flex gap-3 p-1 rounded-xl min-w-[150px] justify-between border-2 border-slate-400">
-        <Navigation
-          href={`${ROUTE_URL.BLOG}${ROUTE_URL.PAGE}/${currentPage - 1}`}
-          text="prev"
-          isEnabled={currentPage < 1}
-        />
-        {pages.map((_, index) => {
-          const page = index + 1
-          const isCurrentPage = page === currentPage
-          const isLastPage = page === totalPages
-          const isFirstPage = page === 1
-          const isWithinRange =
-            page === currentPage ||
-            (page === currentPage - 1 && !isFirstPage) ||
-            (page === currentPage + 1 && !isLastPage)
-
-          return isWithinRange ? (
-            <Link
-              key={page}
-              href={`${PAGE_PATH[type]}/${page}`}
-              className={`p-2 transition-colors duration-300 rounded-lg min-w-[35px] text-center ${
-                isCurrentPage
-                  ? "bg-sky-600 text-neutral-300"
-                  : "hover:text-neutral-300 hover:bg-slate-500/50"
-              }`}
-            >
-              {page}
-            </Link>
-          ) : null
-        })}
-        <Navigation
-          href={`${PAGE_PATH[type]}/${currentPage + 1}`}
-          text="next"
-          isEnabled={currentPage < totalPages}
-        />
-      </div>
-    </div>
-  )
-}
+// Page Component
 
 const Navigation = ({
   href,
@@ -83,3 +37,48 @@ const Navigation = ({
       {text}
     </span>
   )
+
+export const Pagination: React.FC<PaginationProps> = ({
+  currentPage,
+  totalPages,
+  type,
+}) => {
+  const pages = Array.from({ length: totalPages }, (_, idx) => ++idx)
+
+  return (
+    <div className="flex justify-center w-full mt-4">
+      <div className="flex gap-2 p-1 rounded-xl min-w-[150px] justify-between border-2 border-slate-400">
+        <Navigation
+          href={`${ROUTE_URL.BLOG}${ROUTE_URL.PAGE}/${currentPage - 1}`}
+          text="prev"
+          isEnabled={currentPage > 1}
+        />
+        <div className="flex gap-1">
+          {pages.map((page) => {
+            const isCurrentPage = page === currentPage
+
+            return isCurrentPage ? (
+              <span className="py-2 px-0 transition-colors duration-300 rounded-lg min-w-[35px] text-center pointer-events-none   bg-sky-600 text-neutral-300">
+                {page}
+              </span>
+            ) : (
+              <Link
+                key={page}
+                href={`${PAGE_PATH[type]}/${page}`}
+                className="py-2 px-0 transition-colors duration-300 rounded-lg min-w-[35px] text-center hover:text-neutral-300 hover:bg-slate-500/50"
+              >
+                {page}
+              </Link>
+            )
+          })}
+        </div>
+
+        <Navigation
+          href={`${PAGE_PATH[type]}/${currentPage + 1}`}
+          text="next"
+          isEnabled={currentPage < totalPages}
+        />
+      </div>
+    </div>
+  )
+}
