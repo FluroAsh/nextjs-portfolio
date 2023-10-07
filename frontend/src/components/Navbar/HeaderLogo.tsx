bb5f7f2609b7c3220b51d907ce12c3f592cc1ea0
Ashley Thompson <workashleythompson@gmail.com>
2023-10-07 19:44:42 +1100

    refactor: changes logo to have same accent as dark theme

--- frontend/src/components/Navbar/HeaderLogo.tsx
+++ frontend/src/components/Navbar/HeaderLogo.tsx
@@ -8,8 +8,8 @@ const HeaderLogo = ({ className }: { className?: string }) => {
       href="/"
       className={cn(className, "text-3xl tracking-wide font-yellowtail")}
     >
-      <div className="transition-colors duration-300 text-slate-300 dark:text-sky-500">
-        <span className="text-slate-100 dark:text-white">a</span>
+      <div className="transition-colors duration-300 text-sky-500">
+        <span className="text-neutral-100 dark:text-white">a</span>
         <span>|thompson</span>
       </div>
     </Link>
