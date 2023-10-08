1d7aa47e04f36919a191c3596044fddb7c295b6c
Ashley Thompson <workashleythompson@gmail.com>
2023-10-08 22:24:20 +1100

    feat: light theme updates for Blog Post page(s) & light mode anchor #

--- frontend/src/pages/blog/[slug].tsx
+++ frontend/src/pages/blog/[slug].tsx
@@ -38,11 +38,11 @@ const BlogPost: React.FC<BlogPostProps> = ({
     >
       <div className="w-full max-w-screen-lg">
         <header className="w-full px-5">
-          <div className="py-5 border-b border-orange-300 dark:border-slate-500">
+          <div className="py-5 border-b border-neutral-700 dark:border-slate-500">
             <Button
               type="back"
               href={ROUTE_URL.BLOG}
-              className="text-orange-400 transition-colors group hover:text-orange-300 dark:text-neutral-400 dark:hover:text-white"
+              className="transition-colors text-neutral-600 group hover:text-neutral-400 dark:text-neutral-400 dark:hover:text-white"
             >
               <FontAwesomeIcon
                 icon={faArrowLeftLong}
@@ -52,7 +52,7 @@ const BlogPost: React.FC<BlogPostProps> = ({
               <span className="ml-2">Back to Blog</span>
             </Button>
             {/* REVIEW: Should refactor this h1 to use BlogTitle... */}
-            <h1 className="text-3xl font-bold sm:text-4xl text-neutral-800 dark:text-white">
+            <h1 className="text-3xl font-bold tracking-wide sm:text-4xl text-neutral-800 dark:text-white">
               {title}
             </h1>
             <TimeDate
