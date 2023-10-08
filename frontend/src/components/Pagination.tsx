dbd197b0e6962e396b21ba15faff95c6f17c983c
Ashley Thompson <workashleythompson@gmail.com>
2023-10-08 23:04:31 +1100

    feat: updates light theme for Pagination component

--- frontend/src/components/Pagination.tsx
+++ frontend/src/components/Pagination.tsx
@@ -24,12 +24,12 @@ const Navigation = ({
   isEnabled ? (
     <Link
       href={href}
-      className="p-2 capitalize transition-colors duration-300 rounded-lg hover:text-neutral-300 hover:bg-slate-500/50"
+      className="p-2 capitalize transition-colors duration-300 rounded-lg hover:bg-neutral-600/70 hover:text-neutral-200 dark:hover:bg-slate-500/50"
     >
       {text}
     </Link>
   ) : (
-    <span className="p-2 text-gray-400 capitalize pointer-events-none">
+    <span className="p-2 capitalize pointer-events-none text-neutral-600/50 dark:text-neutral-400">
       {text}
     </span>
   )
@@ -49,7 +49,7 @@ export const Pagination: React.FC<PaginationProps<PageType>> = ({
 
   return (
     <div className="flex justify-center w-full mt-4">
-      <div className="flex gap-2 p-1 rounded-xl min-w-[150px] justify-between border-2 border-slate-400">
+      <div className="flex gap-2 p-1 rounded-xl min-w-[150px] justify-between border-2 border-neutral-600 dark:border-slate-400">
         <Navigation
           href={`${PAGE_PATH[type]}/${currentPage - 1}`}
           text="prev"
@@ -62,7 +62,7 @@ export const Pagination: React.FC<PaginationProps<PageType>> = ({
             return isCurrentPage ? (
               <span
                 key={page}
-                className="py-2 px-0 transition-colors duration-300 rounded-lg min-w-[35px] text-center pointer-events-none   bg-sky-600 text-neutral-300"
+                className="py-2 px-0 transition-colors duration-300 rounded-lg min-w-[35px] text-center pointer-events-none bg-sky-600 text-neutral-100 dark:text-neutral-300"
               >
                 {page}
               </span>
@@ -70,7 +70,7 @@ export const Pagination: React.FC<PaginationProps<PageType>> = ({
               <Link
                 key={page}
                 href={`${PAGE_PATH[type]}/${page}`}
-                className="py-2 px-0 transition-colors duration-300 rounded-lg min-w-[35px] text-center hover:text-neutral-300 hover:bg-slate-500/50"
+                className="py-2 px-0 transition-colors duration-300 rounded-lg min-w-[35px] text-center hover:bg-neutral-600/70 hover:text-neutral-200 dark:hover:bg-slate-500/50"
               >
                 {page}
               </Link>
