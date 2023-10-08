94153c9077fdaf1440bde1006b368b1d2224ecd2
Ashley Thompson <workashleythompson@gmail.com>
2023-10-09 00:19:52 +1100

    feat: updates error page for light theme

--- frontend/src/pages/_error.tsx
+++ frontend/src/pages/_error.tsx
@@ -7,17 +7,17 @@ export function Error({ statusCode }: { statusCode: number }) {
   const errorMessage = ERROR_MAP[statusCode]
 
   return (
-    <Layout type="basic">
+    <Layout type="basic" title="ashleygthompson | Error!">
       <div className="flex items-center justify-center dark:text-white -h-navbar">
-        <div className="flex flex-col items-center justify-center p-6 border rounded-lg w-100 dark:bg-slate-500/50 backdrop-blur border-slate-500/50">
+        <div className="flex flex-col items-center justify-center p-6 border rounded-lg w-100 dark:to-slate-500/50 dark:from-slate-400/50 backdrop-blur border-slate-500/50 bg-gradient-to-t to-neutral-400/80 from-neutral-500/80">
           {/* TODO: Replace with an SVG/Img */}
           <span className="mb-4 text-5xl">⚠️</span>
           <div className="flex items-center content-center text-lg">
-            <span className="flex items-center mr-2 after:border-r-2 after:ml-2 after:border-slate-400 after:h-8 after:inline-block">
-              Error {statusCode} &mdash; {errorMessage}
+            <span className="flex items-center mr-2 text-neutral-100 after:border-r-2 after:ml-2 dark:after:border-slate-400 after:border-slate-500/50 after:h-8 after:inline-block">
+              Error 404 &mdash; {errorMessage}
             </span>
             <Link
-              className="transition-colors duration-300 dark:text-slate-300 hover:text-sky-500"
+              className="transition-colors duration-300 text-neutral-100 hover:text-neutral-600 dark:text-slate-300 dark:hover:text-sky-500"
               href="/"
             >
               Go Home
