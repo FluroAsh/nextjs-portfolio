160a09e963cba7f3ca2d364e769b4aa1bd62e25c
Ashley Thompson <workashleythompson@gmail.com>
2023-10-22 00:26:38 +1100

    refactor: updates development database to use Postgres (got sick of accidentally removing all my data 💀)

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -1,16 +1,14 @@
-import path from "path";
-
 export default ({ env }) => ({
   connection: {
-    client: "sqlite",
+    client: "postgres",
     connection: {
-      filename: path.join(
-        __dirname,
-        "..",
-        "..",
-        env("DATABASE_FILENAME", ".tmp/data.db")
-      ),
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi_blog"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "0000"),
+      ssl: false,
+      debug: false,
     },
-    useNullAsDefault: true,
   },
 });
--- backend/dist/config/database.js
+++ backend/dist/config/database.js
@@ -1,15 +1,16 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const path_1 = __importDefault(require("path"));
 exports.default = ({ env }) => ({
     connection: {
-        client: "sqlite",
+        client: "postgres",
         connection: {
-            filename: path_1.default.join(__dirname, "..", "..", env("DATABASE_FILENAME", ".tmp/data.db")),
+            host: env("DATABASE_HOST", "localhost"),
+            port: env.int("DATABASE_PORT", 5432),
+            database: env("DATABASE_NAME", "strapi_blog"),
+            user: env("DATABASE_USERNAME", "postgres"),
+            password: env("DATABASE_PASSWORD", "0000"),
+            ssl: false,
+            debug: false,
         },
-        useNullAsDefault: true,
     },
 });
