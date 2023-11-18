eac1e7be01438b5ceaa118ab4a451f2e1ce94c4a
Ashley Thompson <workashleythompson@gmail.com>
2023-11-18 20:19:50 +1100

    bug(fe): fixes type error for setInterval scrollTimer

--- backend/dist/config/admin.js
+++ backend/dist/config/admin.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = ({ env }) => ({
     auth: {
-        secret: env('ADMIN_JWT_SECRET'),
+        secret: env("JWT_SECRET"),
     },
     apiToken: {
-        salt: env('API_TOKEN_SALT'),
+        salt: env("API_TOKEN_SALT"),
     },
 });
