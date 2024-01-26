00d57c6a79db1f4cb2350d7ee4c58a1f0fa0a1b3
Ashley Thompson <workashleythompson@gmail.com>
2024-01-26 22:42:50 +1100

    bug: fixes Header wrapping issue on mobile

--- frontend/src/components/Navbar/HeaderLogo.tsx
+++ frontend/src/components/Navbar/HeaderLogo.tsx
@@ -6,7 +6,10 @@ const HeaderLogo = ({ className }: { className?: string }) => {
   return (
     <Link
       href="/"
-      className={cn(className, "text-3xl tracking-wide font-yellowtail")}
+      className={cn(
+        className,
+        "text-3xl tracking-wide font-yellowtail whitespace-nowrap"
+      )}
     >
       <div className="transition-colors duration-300 text-sky-500">
         <span className="text-neutral-100 dark:text-white">a</span>
