ee86bda4a45f34a7a10ac3f95ee75d9211fc5df1
Ashley Thompson <workashleythompson@gmail.com>
2024-08-11 22:09:53 +1000

    chore: Update AWS image upload root path to '/images'

--- server/config/plugins.ts
+++ server/config/plugins.ts
@@ -3,6 +3,7 @@ module.exports = ({ env }) => ({
     config: {
       provider: "aws-s3",
       providerOptions: {
+        rootPath: "images",
         accessKeyId: env("AWS_ACCESS_KEY_ID"),
         secretAccessKey: env("AWS_ACCESS_SECRET"),
         region: env("AWS_REGION"),
